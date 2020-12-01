22a028e9af5b350fb96f7bb47be3a275ec66a142
puneet_bajaj <puneet.bajaj@namasys.co>
2020-12-01 18:26:59 +0530

    node completed

--- src/pages/node.jsx
+++ src/pages/node.jsx
@@ -44,40 +44,46 @@ export default class nodePage extends Component {
                 <br />
               </ul>
             </div>
+            <img
+              src="node.png"
+              style={{ position: "absolute", top: "5%", left: "60%" }}
+              alt="nodeJS"
+            />
             <div
               style={{
                 backgroundColor: "#eeeeee",
                 marginTop: "30px",
                 width: "40%",
-                height: "370px",
+                height: "500px",
               }}
             >
               <h1 style={{ backgroundColor: "#4caf50", textAlign: "center" }}>
-                HTTP
+                Create Your Own Modules
               </h1>
               <p style={{ padding: "10px" }}>
-                HTTP is a protocol which allows the fetching of resources, such
-                as HTML documents.
+                You can create your own modules, and easily include them in your
+                applications.
+                <pre>{`
+Create a module that returns the current date and time:
+exports.myDateTime = function () {
+  return Date();
+};
+            `}</pre>
+                Use the exports keyword to make properties and methods available
+                outside the module file and save the code above in a file called
+                "myfirstmodule.js"
+                <pre>{`
+Use the module "myfirstmodule" in a Node.js file:
+var http = require('http');
+var dt = require('./myfirstmodule');
+ 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
+`}</pre>
               </p>
-              <ul>
-                <li>
-                  HTTP/1.1 was first documented in RFC 2068 in 1997. That
-                  specification was obsoleted by RFC 2616 in 1999, which was
-                  likewise replaced by the RFC 7230 family of RFCs in 2014.
-                </li>
-                <br />
-                <li>
-                  HTTP/2 is a more efficient expression of HTTP's semantics "on
-                  the wire", and was published in 2015
-                </li>
-                <br />
-                <li>
-                  HTTP/3 is the proposed successor to HTTP/2, which is already
-                  in use on the web (enabled by default in latest macOS), using
-                  UDP instead of TCP for the underlying transport protocol
-                </li>
-                <br />
-              </ul>
             </div>
             <div
               style={{
@@ -85,8 +91,8 @@ export default class nodePage extends Component {
                 width: "40%",
                 position: "absolute",
                 left: "50%",
-                top: "27%",
-                height: "370px",
+                top: "40%",
+                height: "80%",
               }}
             >
               <h1
@@ -96,76 +102,45 @@ export default class nodePage extends Component {
                   margin: "0",
                 }}
               >
-                Components of a website
+                Example
               </h1>
               <p style={{ padding: "10px" }}>
-                <b>
-                  <u>HTML</u>
-                </b>
-                : This gives functionality to the HTML code written (like the
-                Nervous System) and basically helps the HTML code to be more
-                than just an unintelligible text to an end user and gives it
-                user-friendly access.
+                Example to display "Hello World" in a web browser.
+                <br />
+                <pre>{`
+var http = require('http');
+ 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
+                `}</pre>
+                The code tells the computer to write "Hello World!" if anyone
+                (e.g. a web browser) tries to access your computer on port 8080.
+                <br />
+                <br />
+                Node.js has a built-in module called HTTP, which allows Node.js
+                to transfer data over the Hyper Text Transfer Protocol (HTTP).
+                <pre>
+                  {`
+To include the HTTP module, use the require() method:
+var http = require('http');
+                  `}
+                </pre>
+                The function passed into the <b>http.createServer()</b> method,
+                will be executed when someone tries to access the computer on
+                port 8080.
+                <br />
+                <br />
+                The first argument of the <b>res.writeHead()</b> method is the
+                status code, 200 means that all is OK, the second argument is an
+                object containing the response headers.
+                <br />
+                <br />
+                The argument of the <b>res.end()</b> is the data displayed on
+                screen
               </p>
-              <p style={{ padding: "10px" }}>
-                <b>
-                  <u>Javascript</u>
-                </b>
-                : This gives functionality to the HTML code written (like the
-                Nervous System) and basically helps the HTML code to be more
-                than just an unintelligible text to an end user and gives it
-                user-friendly access.
-              </p>
-              <p style={{ padding: "10px" }}>
-                <b>
-                  <u>CSS</u>
-                </b>
-                : This is the file where all the design elements of a website
-                are defined (like the Muscular System) i.e., the fonts to be
-                used, the colors, the backgrounds, the image sizes etc.
-              </p>
-            </div>
-            <div
-              style={{
-                backgroundColor: "#eeeeee",
-                marginTop: "30px",
-              }}
-            >
-              <h1 style={{ backgroundColor: "#4caf50", textAlign: "center" }}>
-                Front-end vs Backend
-              </h1>
-              <p style={{ padding: "10px" }}>
-                In website design there are generally two types of languages:
-              </p>
-              <p style={{ padding: "10px" }}>
-                <b>
-                  <u>Front End</u>
-                </b>
-                : Languages like HTML, CSS, JS etc. , which help design the
-                webpage which the end-user will be viewing.
-              </p>
-              <p style={{ padding: "10px" }}>
-                <b>
-                  <u>Back End</u>
-                </b>
-                : Languages like Java, SQL, Ruby, PHP etc. , which aid in the
-                server-side programming which holds the databases that the
-                end-user will be accessing through the website.
-              </p>
-              <p style={{ padding: "10px" }}>
-                This division results in three different kinds of
-                web-developers:
-              </p>
-              <ul>
-                <li>
-                  Front End Developers, who work on the front end designing
-                </li>
-                <li>
-                  Back End Developers, who work on the server-side i.e., back
-                  end designing of the databases
-                </li>
-                <li>Full Stack Developers who work on both these stacks.</li>
-              </ul>
             </div>
           </div>
         </>
